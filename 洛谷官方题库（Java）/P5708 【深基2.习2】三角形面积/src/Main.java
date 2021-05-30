82ae7abc301fa18ed0f76b1c653f934a0a63190b
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-05-30 18:30:57 +0800

    P5708 【深基2.习2】三角形面积

---
+++ 洛谷官方题库（Java）/P5708 【深基2.习2】三角形面积/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle tAng = new Triangle();
+
+        if(tAng.getABC()) {
+            System.out.printf("%.1f",tAng.getTriangleArea(tAng.a, tAng.b, tAng.c));
+
+        }
+        else {
+            System.out.println("Error!");
+        }
+    }
+}
+class Triangle {
+    double a, b, c;
+    Scanner sc = new Scanner(System.in);
+
+    public boolean getABC() {
+        a = sc.nextDouble(); b = sc.nextDouble(); c = sc.nextDouble();
+        if(a > 0 && b > 0 && c > 0) {
+            return a + b > c && a + c > b && b + c > a;
+        }
+        return false;
+    }
+
+    public double getTriangleArea(double a, double b, double c) {
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
