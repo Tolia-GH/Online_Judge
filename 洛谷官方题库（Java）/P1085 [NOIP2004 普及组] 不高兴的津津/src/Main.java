a05cb38c0e1d8b4fdd370ddb25b0205e1febb089
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-06-02 09:11:09 +0800

    Update Main.java

--- 洛谷官方题库（Java）/P1085 [NOIP2004 普及组] 不高兴的津津/src/Main.java
+++ 洛谷官方题库（Java）/P1085 [NOIP2004 普及组] 不高兴的津津/src/Main.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-         Child Jingjing = new Child();
+         Child Jinjin = new Child();//实例化津津这个对象
 
-         Jingjing.getSchedule();
-         System.out.println(Jingjing.isHappy());
+         Jinjin.getSchedule();//读入津津的日程表
+         System.out.println(Jinjin.isHappy(8));//计算津津开不开心
     }
 }
 
@@ -13,22 +13,22 @@ class Child {
     int[][] schedule = new int[7][2];
     Scanner sc = new Scanner(System.in);
 
-    public void getSchedule() {
+    public void getSchedule() {//读入小孩的日程表
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 2; j++) {
                 schedule[i][j] = sc.nextInt();
             }
         }
     }
-    public int isHappy() {
+    public int isHappy(int limitHours) {//计算小孩是否开心
         int[] unHappyLevel = new int[7];
         int maxUnhappy_Level = 0;
         int maxUnhappy_Day = 0;
         for (int i = 0; i < 7; i++) {
-            unHappyLevel[i] = schedule[i][0] + schedule[i][1] - 8;
+            unHappyLevel[i] = schedule[i][0] + schedule[i][1] - limitHours;
             if (unHappyLevel[i] > maxUnhappy_Level) {
                 maxUnhappy_Level = unHappyLevel[i];
-                maxUnhappy_Day = i+1;
+                maxUnhappy_Day = i + 1;
             }
         }
         return maxUnhappy_Day;
