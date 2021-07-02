ebc45f4901592672b8e28e649a34054e4074b080
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-07-02 21:50:40 +0800

    update

--- ICSG 团队题库（Java）/test/src/Main.java
+++ ICSG 团队题库（Java）/test/src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(7/2.0);//注释
+        Scanner sc = new Scanner(System.in);
+        int leftNum, rightNum;
+        int[] digit = {0,0,0,0,0,0,0,0,0,0};
+
+        leftNum = sc.nextInt();
+        rightNum = sc.nextInt();
+
+        for(int i = leftNum; i<=rightNum; i++) {
+            int temp = i;
+            while(temp!=0) {
+                digit[temp%10]++;
+                temp/=10;
+            }
+        }
+
+        for(int i:digit) {
+            System.out.print(i+" ");
+        }
     }
 }
