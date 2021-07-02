111e7ec4327c5a2869a302bc362acdeaeea6759d
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-07-02 20:04:05 +0800

    update

--- ICSG 团队题库（Java）/test/src/Main.java
+++ ICSG 团队题库（Java）/test/src/Main.java
@@ -1,19 +1,5 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=0;
-        for(int i=1;i<=b;i++) {
-            if(a!=6 && a!=7) {
-                c=c+250;
-            }
-            if(a==8) {
-                a=1;
-            }
-            a++;
-        }
-        System.out.println(c);
+        System.out.println(7/2.0);
     }
 }
