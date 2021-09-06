58e6e722076043099959a8439df7f0e9dbf240c9
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-09-06 16:08:34 +0800

    up

---
+++ .test/src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date deadline = new Date();
+        Date birthday = deadline;
+        String s = deadline.toString();
+    }
+}
